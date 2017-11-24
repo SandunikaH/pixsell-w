799758eac03c5e3d2f059dc7c8550112216e3671
sandunika <sandunikahansinie@gmail.com>
2017-11-24 10:33:36 +0530

    signUp, signIn, resetPassword views are added

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+// Initialize Firebase
+export var config = {
+  apiKey: "AIzaSyCzedvPPYg5aIJAyVgItXXwA0aa9KmHC34",
+  authDomain: "pixsell-w.firebaseapp.com",
+  databaseURL: "https://pixsell-w.firebaseio.com",
+  projectId: "pixsell-w",
+  storageBucket: "pixsell-w.appspot.com",
+  messagingSenderId: "11111491730"
+};
+firebase.initializeApp(config);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -27,22 +38,37 @@ app.get('/', function(request, response) {
   response.render('metlook/index',{baseFrame:baseFrame,sideMenu:sideMenu});
 });
 
-app.get('/webSiteOwner', function(request, response) {
+// app.get('/webSiteOwner', function(request, response) {
   
-  var baseFrame = 
-    {title: 'Web Site Owner',
-     userName: 'Tharindu',
-     siteName: ['AB.lk','bc.lk'],
-     userImage:'images/user.png'
-    };
-  var sideMenu =
-    [
-      {name:'Home',icon:'home',link:'Home',state:'active'},
-      {name:'RegisteAd-block',icon:'layers',link:'reg_adblock'},
-      {name:'Earnings',icon:'monetization_on',link:'earnings'}
-    ]
+//   var baseFrame = 
+//     {title: 'Web Site Owner',
+//      userName: 'Tharindu',
+//      siteName: ['AB.lk','bc.lk'],
+//      userImage:'images/user.png'
+//     };
+//   var sideMenu =
+//     [
+//       {name:'Home',icon:'home',link:'Home',state:'active'},
+//       {name:'RegisteAd-block',icon:'layers',link:'reg_adblock'},
+//       {name:'Earnings',icon:'monetization_on',link:'earnings'}
+//     ]
 
-  response.render('metlook/pages/webSiteOwner',{baseFrame:baseFrame,sideMenu:sideMenu});
+//   response.render('metlook/pages/webSiteOwner',{baseFrame:baseFrame,sideMenu:sideMenu});
+// });
+
+app.get('/signIn', function (request, response) {
+  console.log('login page')
+  response.render('metlook/pages/signIn');
+});
+
+app.get('/signUp', function (request, response) {
+  console.log('create account page')
+  response.render('metlook/pages/signUp');
+});
+
+app.get('/forgotPassword', function (request, response) {
+  console.log('reset password page')
+  response.render('metlook/pages/forgotPassword');
 });
 
 app.listen(app.get('port'), function() {
